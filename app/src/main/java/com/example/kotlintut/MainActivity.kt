f26297f998023f645081e2de2a1341aec6830ea1
llcain <llcain2012@gmail.com>
2019-10-19 02:40:09 -0500

    Kotlin-tut-solutions

---
+++ app/src/main/java/com/example/kotlintut/MainActivity.kt
@@ -0,0 +1,164 @@
+package com.example.kotlintut
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    internal var totalSoFar = 0
+
+     //internal var myACounter = 0
+     //internal var myBCounter = 0
+
+    fun total():Int {
+        return totalSoFar
+    }
+
+    fun scan (good:Char) {
+        println("\nThe character is: " + good)
+
+        totalSoFar = totalSoFar + 50
+
+    }
+
+     // totalSoFar = totalSoFar + 50
+
+//    if (good == 'A')
+//    {
+//        totalSoFar = totalSoFar + 50
+//    }
+//    else
+//    {
+//        totalSoFar = totalSoFar + 30
+//    }
+
+//    when (good) {
+//        'A' -> totalSoFar = totalSoFar + 50
+//        'B' -> totalSoFar = totalSoFar + 30
+//        'C' -> totalSoFar = totalSoFar + 20
+//        'D' -> totalSoFar = totalSoFar + 15
+//    }
+
+//    when (good) {
+//        'A' -> {
+//            myACounter = myACounter + 1
+//            if (myACounter === 3)
+//            {
+//                totalSoFar = totalSoFar + 30
+//                myACounter = 0
+//            }
+//            else
+//            {
+//                totalSoFar = totalSoFar + 50
+//            }
+//        }
+//        'B' -> totalSoFar = totalSoFar + 30
+//        'C' -> totalSoFar = totalSoFar + 20
+//        'D' -> totalSoFar = totalSoFar + 15
+//    }
+
+//    when (good) {
+//        'A' -> {
+//            myACounter = myACounter + 1
+//            if (myACounter === 3)
+//            {
+//                totalSoFar = totalSoFar + 30
+//                myACounter = 0
+//            }
+//            else
+//            {
+//                totalSoFar = totalSoFar + 50
+//            }
+//        }
+//        'B' -> totalSoFar = totalSoFar + 30
+//        'C' -> totalSoFar = totalSoFar + 20
+//        'D' -> totalSoFar = totalSoFar + 15
+//    }
+
+    //    when (good) {
+//        'A' -> {
+//            myACounter = myACounter + 1
+//            if (myACounter === 3)
+//            {
+//                totalSoFar = totalSoFar + 30
+//                myACounter = 0
+//            }
+//            else
+//            {
+//                totalSoFar = totalSoFar + 50
+//            }
+//        }
+//        'B' -> totalSoFar = totalSoFar + 30
+//        'C' -> totalSoFar = totalSoFar + 20
+//        'D' -> totalSoFar = totalSoFar + 15
+//    }
+
+    //    when (good) {
+//        'A' -> {
+//            myACounter = myACounter + 1
+//            if (myACounter === 3)
+//            {
+//                totalSoFar = totalSoFar + 30
+//                myACounter = 0
+//            }
+//            else
+//            {
+//                totalSoFar = totalSoFar + 50
+//            }
+//        }
+//        'B' -> totalSoFar = totalSoFar + 30
+//        'C' -> totalSoFar = totalSoFar + 20
+//        'D' -> totalSoFar = totalSoFar + 15
+//    }
+
+    //    when (good) {
+//        'A' -> {
+//            myACounter = myACounter + 1
+//            if (myACounter === 3)
+//            {
+//                totalSoFar = totalSoFar + 30
+//                myACounter = 0
+//            }
+//            else
+//            {
+//                totalSoFar = totalSoFar + 50
+//            }
+//        }
+//        'B' -> totalSoFar = totalSoFar + 30
+//        'C' -> totalSoFar = totalSoFar + 20
+//        'D' -> totalSoFar = totalSoFar + 15
+//    }
+
+//    when (good) {
+//        'A' -> {
+//            myACounter = myACounter + 1
+//            if (myACounter === 3)
+//            {
+//                totalSoFar = totalSoFar + 30
+//                myACounter = 0
+//            }
+//            else
+//            {
+//                totalSoFar = totalSoFar + 50
+//            }
+//        }
+//        'B' -> {
+//            myBCounter = myBCounter + 1
+//            if (myBCounter === 2)
+//            {
+//                totalSoFar = totalSoFar + 15
+//            }
+//            else
+//            {
+//                totalSoFar = totalSoFar + 30
+//            }
+//        }
+//        'C' -> totalSoFar = totalSoFar + 20
+//        'D' -> totalSoFar = totalSoFar + 15
+//    }
+
+
+
+
+
+}
---
+++ app/src/test/java/com/example/kotlintut/MainActivityTest.kt
@@ -0,0 +1,151 @@
+package com.example.kotlintut
+
+import org.junit.Assert.*
+import org.junit.rules.ExpectedException
+import com.example.kotlintut.MainActivity as ComExampleKotlintutMainActivity
+import com.example.kotlintut.MainActivityTest as MainActivityTest1
+import org.junit.Test as JunitTest
+import kotlin.Int as Int1
+
+class MainActivityTest<Int1> {
+
+    @JunitTest
+    @Throws(Exception::class)
+    fun priceIsZeroWhenNoGoods() {
+        val noItems = ""
+        val price = price(noItems)
+        assertEquals(0, price(noItems))
+
+    }
+
+
+    private fun price(goods: CharSequence): kotlin.Int {
+        val mainActivityTest = ComExampleKotlintutMainActivity()
+        for (i in 0 until goods.length) {
+            val good = goods.get(i)
+            mainActivityTest.scan(good)
+
+        }
+
+        return mainActivityTest.total()
+    }
+
+    @JunitTest
+    @Throws(Exception::class)
+    fun priceIsFiftyWhenGoodIsA() {
+        val items = "A"
+        val price = price(items)
+        assertEquals(50, price)
+    }
+
+//    @JunitTest
+//    @Throws(Exception::class)
+//    fun priceIsEightyWhenGoodIsA() {
+//        val items = "AB"
+//        val price = price(items)
+//        assertEquals(80, price)
+//    }
+
+//    @JunitTest
+//    @Throws(Exception::class)
+//    fun priceIsOneHundredFifteenWhenGoodAreCDBA() {
+//        val items = "CDBA"
+//        val price = price(items)
+//        assertEquals(115, price)
+//    }
+//
+//    //Section 1 ends
+//    //Section 2 starts
+//
+//    @JunitTest
+//    @Throws(Exception::class)
+//    fun priceIsOneHundredWhenGoodAreAA() {
+//        val items = "AA"
+//        val price = price(items)
+//        assertEquals(100, price)
+//    }
+//
+//    @JunitTest
+//    @Throws(Exception::class)
+//    fun priceIsOneHundredThirtyWhenGoodAreAAA() {
+//        val items = "AAA"
+//        val price = price(items)
+//        assertEquals(130, price)
+//    }
+//
+//    @JunitTest
+//    @Throws(Exception::class)
+//    fun priceIsOneHundredEightyWhenGoodAreAAAA() {
+//        val items = "AAAA"
+//        val price = price(items)
+//        assertEquals(180, price)
+//    }
+//
+//    @JunitTest
+//    @Throws(Exception::class)
+//    fun priceIsTwoHundredThirtyWhenGoodAreAAAAA() {
+//        val items = "AAAAA"
+//        val price = price(items)
+//        assertEquals(230, price)
+//    }
+//
+//    @JunitTest
+//    @Throws(Exception::class)
+//    fun priceIsTwoHundredSixtyWhenGoodAreAAAAAA() {
+//        val items = "AAAAAA"
+//        val price = price(items)
+//        assertEquals(260, price)
+//    }
+//
+//    //Section 2 ends
+//    //Section 3 begins
+//
+//    @JunitTest
+//    @Throws(Exception::class)
+//    fun priceIsOneHundredSixtyWhenGoodAreAAAB() {
+//        val items = "AAAB"
+//        val price = price(items)
+//        assertEquals(160, price)
+//    }
+//
+//    @JunitTest
+//    @Throws(Exception::class)
+//    fun priceIsOneHundredNinetyWhenGoodAreAAABBD() {
+//        val items = "AAABBD"
+//        val price = price(items)
+//        assertEquals(190, price)
+//    }
+//
+//    @JunitTest
+//    @Throws(Exception::class)
+//    fun priceIsOneHundredNinetyWhenGoodAreDABABA() {
+//        val items = "DABABA"
+//        val price = price(items)
+//        assertEquals(190, price)
+//    }
+//
+//    //Section 3 ends
+//    //Section 4 starts
+
+
+
+@JunitTest
+    fun `getTotalSoFar$app`() {
+    }
+
+    @JunitTest
+    fun `setTotalSoFar$app`() {
+    }
+
+    @JunitTest
+    fun total() {
+    }
+
+    @JunitTest
+    fun scan() {
+    }
+
+
+}
+
+
